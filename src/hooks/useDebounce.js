767d233612b61431e30d3c10633b53a92bddf320
Hoang Quan <siluq.quan.1609@gmail.com>
2026-04-14 23:40:15 +0700

    fix api user

--- src/hooks/useDebounce.js
+++ src/hooks/useDebounce.js
@@ -2,9 +2,13 @@ import { useState, useEffect } from 'react'
 
 export function useDebounce(value, delay = 400) {
   const [debounced, setDebounced] = useState(value)
+  const debouncedId = useDebounce(userId, 400);
   useEffect(() => {
-    const t = setTimeout(() => setDebounced(value), delay)
-    return () => clearTimeout(t)
-  }, [value, delay])
+    if (debouncedId) { 
+      userService.getBookings(debouncedId)
+        .then(res => setBookings(res.data))
+        .catch(err => console.error(err));
+    }
+  }, [debouncedId]);
   return debounced
 }
